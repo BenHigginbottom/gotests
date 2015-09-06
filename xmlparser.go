c35f93ed6ee29d4fe696095a459886becc943048
benhigginbottom <ben.higginbottom@gmail.com>
2015-09-06 23:16:10 +0100

    Added error handling to the file open and read

--- xmlparser.go
+++ xmlparser.go
@@ -14,8 +14,18 @@ type XML struct {
 
 func main() {
 	x := XML{}
-	xmlFile := os.Open("/home/ben/Media/Code/go/gotests/test.xml")
-	xmlData, _ := ioutil.ReadAll(xmlFile)
+	xmlFile, err := os.Open("/home/ben/Media/Code/go/gotests/test.xml")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	xmlData, err := ioutil.ReadAll(xmlFile)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
 	
 	xml.Unmarshal(xmlData, x)
 	fmt.Printf("foo: %s\n", x.Foo)
